db204b43c1454aba5e66da0b2f0c6378554c8c9b
Valentin <111140062+ValentinGolban99@users.noreply.github.com>
2022-09-15 09:11:53 +0300

    47 промежуточное сохранение

---
+++ task_47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте двухмерный массив размером m*n, заполненный случайными вещественными числами.
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] GetMatrix(int m, int n, int minRandom, int maxRandom)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            matrix[i, j] = new Random().Next(minRandom, maxRandom + 1);
+        }
+    }
+    return matrix;
+}
+// Напечатать массив
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++)
+        {
+            Console.Write(inputMatrix[i, j] + "\t"); 
+        }
+    Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetMatrix(rows, columns, 0, 10);
+PrintMatrix(resultMatrix);
